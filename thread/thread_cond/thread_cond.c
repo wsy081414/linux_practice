8392257b093118398283e8a1d61cb695fb5056f5
wsy081414 <953659912@qq.com>
2017-02-19 22:17:56 -0800

    2-20

--- thread/thread_cond/thread_cond.c
+++ thread/thread_cond/thread_cond.c
@@ -88,13 +88,12 @@ void * producer_run(void *arg)
     node_p list=(node_p)arg;    
     while(1)
     {
-        sleep(1);
         pthread_mutex_lock(&mylock);
         int data=rand()%1000;
         push_head(list,data);
-        pthread_cond_signal(&mycond);
         pthread_mutex_unlock(&mylock);
         printf("producer: data:%d\n",data);
+        pthread_cond_signal(&mycond);
     }
 
 }
@@ -106,9 +105,9 @@ void * consumer_run(void *arg)
     {
         pthread_mutex_lock(&mylock);
         while(list->next==NULL)
-        {
-            pthread_cond_wait(&mycond,&mylock);
-        }
+      {
+          pthread_cond_wait(&mycond,&mylock);
+      }
         int data=0;
         pthread_mutex_unlock(&mylock);
         pop_front(list,&data);
@@ -121,15 +120,18 @@ int main()
 {
     node_p list=NULL;
     init_list(&list);
-    printf("init_list cuccessi\n");
+    printf("init_list cuccess\n");
     pthread_t proid;
     pthread_t conid;
     
-    pthread_create(&proid,NULL,producer_run,(void *)list);
     pthread_create(&conid,NULL,consumer_run,(void *)list);
+    pthread_create(&proid,NULL,producer_run,(void *)list);
     
     pthread_join(proid,NULL);
     pthread_join(conid,NULL);
+
+    pthread_mutex_destroy(&mylock);
+    pthread_cond_destroy(&mycond);
 
     destory_list(list);
     return 0;
