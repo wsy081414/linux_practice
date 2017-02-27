aec88d0fd89105559b4d4d276f3332ec16c605ef
wsy081414 <953659912@qq.com>
2017-02-26 16:25:48 -0800

    2_23

--- daemon/mydaemon2/mydaemon.c
+++ daemon/mydaemon2/mydaemon.c
@@ -6,25 +6,33 @@
 #include<signal.h>
 void mydaemon()
 {
+    //设置掩码
     umask(0);
 
+    //父进程进行退出
     if(fork()>0)
     {
         exit(2);
     }
+    //创建新的会话
     setsid();
+    
+    //捕捉SIGCHLD信号，操作为忽略
+    signal(SIGCHLD,SIG_IGN);
 
+    //进行第二次fork()，为了防止新的紫禁城再次进行调用控制终端
     if(fork()>0)
     {
         exit(2);   
     }
-    chdir("/");
 
+    //改变文件目录
+    chdir("/");
+    //关闭文件描述符
     close(0);
     close(1);
     close(2);
 
-    signal(SIGCHLD,SIG_IGN);
 }
 int main()
 {
