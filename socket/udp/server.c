3b7564eb763bcc2d6f1fd4b7174bf6c85287aec1
wsy081414 <953659912@qq.com>
2017-03-11 03:55:36 -0800

    3_11

---
+++ socket/udp/server.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int argc,char *argv[])
+{
+    if(argc != 3)
+    {
+        printf("Usage %s [ip] [port] \n",argv[0]);
+        return 1;
+    }
+
+    int sock = socket(AF_INET,SOCK_DGRAM,0);
+    if(sock < 0)
+    {
+        perror("socket");
+        return 2;
+    }
+    
+    struct sockaddr_in local;
+    local.sin_family = AF_INET;
+    local.sin_port = htons(atoi(argc[2]));
+    local.sin_addr.s_addr = inet_addr(argc[1]);
+
+    if(bind(sock,(struct sockaddr *)&local,sizeof(local)) < 0)
+    {
+        perror("bind");
+        return 3;
+    }
+    
+    while(1)
+    {
+        printf("Client send#\n");
+        
+        
+    }
+
+    struct_
+    return 0;
+}
